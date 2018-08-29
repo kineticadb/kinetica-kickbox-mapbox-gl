632e5dc3d1cda7a8c7dc1ec8bf3fdbac638b92cd
Matt Brown <mbrown@kinetica.com>
2018-08-28 21:29:00 -0600

    0.13.3 fix: Adds Missing UMD Version Adds missing UMD version of library to the dist folder.

---
+++ build/webpack.config.umd.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+  filename: 'style.css'
+});
+
+const varConfig = {
+  context: path.resolve(__dirname, '../src'),
+  entry: {
+    app: ['babel-polyfill', './js/kickbox.js']
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '../src/')
+    }
+  },
+  output: {
+    library: 'kickbox',
+    filename: 'kickbox.umd.min.js',
+    path: path.resolve(__dirname, '../dist'),
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      { test: /\.html$/, use: ['html-loader'] },
+      { test: /\.handlebars$/, loader: 'handlebars-loader' },
+      { test: /\.scss$/, loader: 'style!css!sass' },
+      { test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env'] }
+        }
+      }
+    ]
+  },
+  plugins: [
+    extractSass,
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+};
+
+// #endregion Module Export
+
+//////////////////////////////
+// Module Exports
+//////////////////////////////
+
+module.exports = [varConfig];
+
+// #endregion Module Exports
